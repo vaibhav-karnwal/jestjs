89384b45978c729eed0d713b2ec2e7b54f90ac3b
Vaibhav Karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2022-01-20 11:14:16 +0530

    Add files via upload

---
+++ sum.test.js
@@ -0,0 +1,47 @@
+const sum = require('./sum');
+
+//check output sum of numbers
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+//check whether number is null, defined, undefined, falsy
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+//check whether 2+2 == 4 or not
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+  
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
+
+
+//check whether 2 floating number sum equal to output value or not
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+});
+
